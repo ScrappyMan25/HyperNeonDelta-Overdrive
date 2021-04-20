fe153ca7ee711e7535c8297391e2ec795f0d49d5
ScrappyMan25 <53944571+ScrappyMan25@users.noreply.github.com>
2021-04-20 10:27:38 +0100

    Removed test case from enemy

--- Game/Scripts/Enemy.gd
+++ Game/Scripts/Enemy.gd
@@ -5,17 +5,9 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-var la : int 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
-#	pass
-
-
-func test():
-	print("Test")
-	pass
-
 
 func _on_Enemy_body_entered(body: Node) -> void:
 	if "Player" in body.name:
